08896a5f2c77215b06810d9aed51607405d1946c
Rishav <rishavsinha380@gmail.com>
2022-07-29 21:32:18 +0530

    use colors npm package

---
+++ colors_npm.js
@@ -0,0 +1,12 @@
+//Using colors npm
+const colors = require('colors');
+
+console.log("Hii".green);//text colors
+console.log("Rishav".brightRed);//bright text colors
+console.log("package.json".bgYellow);//background colors
+console.log("Hello".bgBrightMagenta);//bright background color
+console.log("I".inverse);//styles
+console.log("am".rainbow);//extras
+console.log("Fine".america);//extras
+console.log('i like cake and pies'.underline.cyan) //styles  outputs red underlined text 
+console.log("Jackson".strikethrough);
